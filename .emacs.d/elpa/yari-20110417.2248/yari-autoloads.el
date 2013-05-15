516a02718b7694114020f51388ff15256733ccd0
Jake Plimack <Jake.plimack@gmail.com>
2013-05-15 00:15:57 -0400

    emacs configs dont live here no mo

--- .emacs.d/elpa/yari-20110417.2248/yari-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; yari-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (yari yari-anything) "yari" "yari.el" (20762 31079
-;;;;;;  0 0))
-;;; Generated autoloads from yari.el
-
-(autoload 'yari-anything "yari" "\
-
-
-\(fn &optional REHASH)" t nil)
-
-(autoload 'yari "yari" "\
-Look up Ruby documentation.
-
-\(fn &optional RI-TOPIC REHASH)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("yari-pkg.el") (20762 31079 14296 0))
-
-;;;***
-
-(provide 'yari-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; yari-autoloads.el ends here
--- .emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-;;; init.el --- Where all the magic begins
-;;
-;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
-;; embedded in literate Org-mode files.
-
-;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
-(require 'org-install)
-(require 'ob-tangle)
-(org-babel-load-file "~/.emacs.d/emacs.org")
