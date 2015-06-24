c02a9182ee5f9367ef6afea51b896d47c8095daf
Jake Plimack <jake.plimack@gmail.com>
2015-06-24 11:02:51 -0700

    fix emacs build to use brew unless --source is specified

--- setterupper.sh
+++ setterupper.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+sudo -v
+
 if [ ! -z $DEBUG ] ; then
 #    set -x
     set -e
+fi
+
+if [ "$1" -eq "--source"] ; then
+    INSTALL='source'
+else
+    INSTALL='brew'
 fi
 
 DOTFILES_DIR="$HOME/repos/dotfiles"; SCRIPT_PATH=$(eval "cd \"$SCRIPT_PATH\" && pwd")
@@ -22,9 +30,39 @@ done
 
 cd ~/.oh-my-zsh
 git submodule update --init
+echo "ENTER YOUR SYSTEM PASSWORD: "
 chsh -s $(which zsh)
 
+# homebrew with xcode!
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install pv tree autoconf automake
+
 # emacs setup
+
+if [ $INSTALL == 'source' ]; then
+    
+    if [ -e /usr/local/share/emacs ] ; then
+	sudo rm -rf /usr/local/share/emacs
+    fi
+    
+    cd repos
+    git clone -b emacs-24 git@github.com:emacs-mirror/emacs.git
+    cd emacs
+    ./autogen.sh
+    ./configure --without-x --with-ns
+    make && sudo make install
+elif [ $INSTALL == 'brew' ]; then
+    brew install --with-cocoa --srgb emacs
+    brew linkapps emacs
+fi
+
 mkdir -p $HOME/.emacs.d/personal/savefile/
 touch $HOME/.emacs.d/personal/savefile/savehist
 touch $HOME/.password.el.gpg
+
+if [ ! -d /etc/profile.d ] ; then
+    sudo mkdir -p /etc/profile.d
+    sudo touch /etc/profile.d/jake.sh
+fi
+
+zsh
